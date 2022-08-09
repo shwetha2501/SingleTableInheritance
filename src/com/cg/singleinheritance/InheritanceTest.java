7b715033fe327a9b1fe6120eed299a602f58c537
swetha <shwethabs25@gmail.com>
2022-08-09 09:36:33 +0530

    first commit

---
+++ src/com/cg/singleinheritance/InheritanceTest.java
@@ -0,0 +1,43 @@
+package com.cg.singleinheritance;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+	
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em=factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		//Created One Employee
+		Employee employee = new Employee();
+		employee.setName("Lekha");
+		employee.setSalary(5000);
+		em.persist(employee);
+		
+		//Created One Manager
+		Manager manager= new Manager();
+		manager.setName("Vikas");
+		manager.setSalary(6000);
+		manager.setDepartmentName("C2TC");
+		em.persist(manager);
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added One Employee and Manager");
+		
+		em.close();
+		
+		factory.close();
+
+	}
+
+}
---
+++ src/com/cg/singleinheritance/Manager.java
@@ -0,0 +1,22 @@
+package com.cg.singleinheritance;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("MGR")
+public class Manager extends Employee {
+	
+	private String DepartmentName;
+
+	public String getDepartmentName() {
+		return DepartmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		DepartmentName = departmentName;
+	}
+	
+	
+
+}
